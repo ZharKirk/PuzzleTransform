56172b02c05201eeab1516bc413e7cf3030886e5
zkirk <zhar.kirk@gmail.com>
2022-03-02 12:01:05 +0300

    TriggerPlatform added

--- Source/PuzzleTransform/PlatformTrigger.cpp
+++ Source/PuzzleTransform/PlatformTrigger.cpp
@@ -3,31 +3,28 @@
 
 #include "PlatformTrigger.h"
 
+#include "Components\BoxComponent.h"
+
+//if (!ensure(True)) return;
+
 // Sets default values
 APlatformTrigger::APlatformTrigger()
 {
- 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
-	//emMeshComp = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("ItemMeshComponent"));
-	//ItemMeshComp->SetSimulatePhysics(true);
-	//ItemMeshComp->SetupAttachment(RootComponent);
-	//RootComponent = ItemMeshComp;
-
-
+	TriggerVolume = CreateDefaultSubobject<UBoxComponent>(TEXT("TriggerVolume"));
+	if (!ensure(TriggerVolume != nullptr)) return;
+	RootComponent = TriggerVolume;
 }
 
 // Called when the game starts or when spawned
 void APlatformTrigger::BeginPlay()
 {
-	Super::BeginPlay();
-	
+	Super::BeginPlay();	
 }
 
 // Called every frame
 void APlatformTrigger::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
-
 }
-
